2b01ea9681b1fd951ceebc22b6d553641cea79d5
rodzy <irod2899@gmail.com>
2020-10-04 05:27:30 +0100

    Meta tags

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,26 @@ import { Canvas } from "react-three-fiber";
 import TheFooter from "../components/TheFooter";
 import styles from "../styles/Home.module.css";
 import Box from "../components/Box";
-// import Model from '../components/Model'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Blocks</title>
+        <meta
+          name="description"
+          content="Short example of Three.js usage with Next.js"
+        ></meta>
+        <meta name="og:site_name" content="Blocks by Rod" />
+        <meta name="og:title" content="Blocks by Rod" />
+        <meta
+          name="og:description"
+          content="Short example of Three.js usage with Next.js"
+        />
+        <meta
+          property="og:image"
+          content="https://avatars1.githubusercontent.com/u/49137701?s=400&u=35e7614cc3215c045b58b4e352bd0616b9d795fd&v=4"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
